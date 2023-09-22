ee9e6e46392e2a395c43cf1ffd087f285f0f42b8
MiladLiri <milad.liri@yahoo.com>
2023-09-22 10:11:39 +0330

    add font settings

---
+++ src/Core/Implementations/Image/Settings/FontSettings.cs
@@ -0,0 +1,10 @@
+using SixLabors.Fonts;
+
+namespace WonderfulCaptcha.Images.Settings;
+
+public static class FontSettings
+{
+    public const string DefaultFont = "Arial";
+
+    public const FontStyle DefaultFontStyle = FontStyle.Regular;
+}
