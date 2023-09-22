7e01ac41108c675d34d57ccdf725e9c8c9d92cab
MiladLiri <milad.liri@yahoo.com>
2023-09-22 16:51:41 +0330

    add brush enum and utils

---
+++ src/Core/Enums/BrushEnum.cs
@@ -0,0 +1,13 @@
+namespace WonderfulCaptcha;
+
+public enum BrushEnum
+{
+    Percent20 = 1,
+    BackwardDiagonal,
+    ForwardDiagonal,
+    Horizontal,
+    Min,
+    Vertical,
+    Solid,
+    Random
+}
---
+++ src/Core/Implementations/Image/Utils/BrushUtils.cs
@@ -0,0 +1,21 @@
+using SixLabors.ImageSharp.Drawing.Processing;
+
+namespace WonderfulCaptcha.Images;
+
+public class BrushUtils
+{
+    private static readonly Random Random = new Random();
+    
+    public static Brush GetBrush(BrushEnum brush, Color color) =>
+        brush switch
+        {
+            BrushEnum.Percent20 => Brushes.Percent20(color),
+            BrushEnum.BackwardDiagonal => Brushes.BackwardDiagonal(color),
+            BrushEnum.ForwardDiagonal => Brushes.ForwardDiagonal(color),
+            BrushEnum.Horizontal => Brushes.Horizontal(color),
+            BrushEnum.Min => Brushes.Min(color),
+            BrushEnum.Vertical => Brushes.Vertical(color),
+            BrushEnum.Solid => Brushes.Solid(color),
+            _ => GetBrush((BrushEnum)Random.Next(8), color),
+        };
+}
